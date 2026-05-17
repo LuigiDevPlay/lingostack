901ca91fb6b65a671e76d4df1ff7be56dedc22fc
LuigiDevPlay <lg6347697@gmail.com>
2026-05-17 14:21:37 -0400

    pc y movil pantalla

--- src/game/main.js
+++ src/game/main.js
@@ -21,8 +21,8 @@ const config = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: "100%",
-    height: "100%",
+    width: window.innerWidth,
+    height: window.innerHeight,
   },
   physics: {
     default: "arcade",
